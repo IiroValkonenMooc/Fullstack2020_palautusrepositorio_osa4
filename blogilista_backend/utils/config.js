165fc34adc01079bb210debdb0627d6737222322
IiroValkonenMooc <62344585+IiroValkonenMooc@users.noreply.github.com>
2020-06-27 05:38:11 +0300

    supertest pohjustus valmis

--- blogilista_backend/utils/config.js
+++ blogilista_backend/utils/config.js
@@ -1,7 +1,11 @@
 require('dotenv').config()
 
-const mongoDbUrl = process.env.MONGODB_URI
+let mongoDbUrl = process.env.MONGODB_URI
 const port = process.env.PORT
+
+if(process.env.NODE_ENV === 'test'){
+    mongoDbUrl = process.env.MONGODB_URI_TEST
+}
 
 module.exports = {
     mongoDbUrl,
