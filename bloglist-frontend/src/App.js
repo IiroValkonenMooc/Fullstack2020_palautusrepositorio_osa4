491aed3862061284650aae8390c674afeef034c3
IiroValkonenMooc <62344585+IiroValkonenMooc@users.noreply.github.com>
2020-07-10 22:48:33 +0300

    ui nappuloita ja kenttiä paikallaan

--- bloglist-frontend/src/App.js
+++ bloglist-frontend/src/App.js
@@ -4,6 +4,8 @@ import blogService from './services/blogs'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [loginName, setLoginName] = useState('')
+  const [loginPw, setLoginPw] = useState('')
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -11,8 +13,30 @@ const App = () => {
     )  
   }, [])
 
+  const handleLoginNameChange = (event) => {
+    setLoginName(event.target.value)
+  }
+
+  const handleLoginPwChange = (event) => {
+    setLoginPw(event.target.value)
+  }
+
   return (
     <div>
+      <h2>Log into application</h2>
+      <div>
+        {'Username'} <br></br>
+        <input type="text" name='username' value={loginName} onChange={handleLoginNameChange} />
+      </div>
+      <div>
+        {'Password'} <br></br>
+        <input type="text" name='password' value={loginPw} onChange={handleLoginPwChange} />
+      </div>
+      <div>
+        <button name='loginbutton'>
+          Login
+        </button>
+      </div>
       <h2>blogs</h2>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
